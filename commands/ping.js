b3f2d2ad2ebec1ff389b0ac2d4d58a8f9b6c38ac
BatuKawaiiAnimes <160671092+BatuKawaiiAnimes@users.noreply.github.com>
2024-02-21 12:08:27 -0300

    Update ping.js
    tradución

--- commands/ping.js
+++ commands/ping.js
@@ -17,7 +17,7 @@ const { EmbedBuilder } = require('discord.js')
 const db = require("../mongoDB");
 module.exports = {
   name: "ping",
-  description: "check the bot latency",
+  description: "Calculando la latencia",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
@@ -26,11 +26,11 @@ module.exports = {
     try {
 
       const start = Date.now();
-      interaction.reply("Pinging....").then(msg => {
+      interaction.reply("Calculando....").then(msg => {
         const end = Date.now();
         const embed = new EmbedBuilder()
           .setColor(`#6190ff`)
-          .setTitle(`Bot Latency`)
+          .setTitle(`Latencia`)
           .setDescription(`**Pong** : ${end - start}ms`)
         return interaction.editReply({ embeds: [embed] }).catch(e => { });
       }).catch(err => { })
